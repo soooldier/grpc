8c3ad02ad5960fb54c5e4f0975fab94765dc79ab
tangyi <tangyi3@staff.weibo.com>
2020-12-17 16:24:05 +0800

    update

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/soooldier/grpc
 go 1.13
 
 require (
-	github.com/EDDYCJY/go-grpc-example v0.0.0-20181014074047-0f68708edbcb // indirect
 	github.com/golang/protobuf v1.4.3
+	github.com/grpc-ecosystem/go-grpc-middleware v1.2.2
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.0.1
 	google.golang.org/grpc v1.34.0
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.0.1
 	google.golang.org/protobuf v1.25.0
 )
--- server/simple/server.go
+++ server/simple/server.go
@@ -4,10 +4,14 @@ import (
 	"context"
 	"log"
 	"net"
+	"runtime/debug"
 
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	pb "github.com/soooldier/grpc/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
 type SearchService struct{}
@@ -18,8 +22,31 @@ func (s *SearchService) Search(ctx context.Context, r *pb.SearchRequest) (*pb.Se
 
 const PORT = "9001"
 
+func RecoveryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			debug.PrintStack()
+			err = status.Errorf(codes.Internal, "Panic error: %v", e)
+		}
+	}()
+	return handler(ctx, req)
+}
+
+func LoggingInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	log.Printf("gRPC method: %s, %v", info.FullMethod, req)
+	resp, err = handler(ctx, req)
+	log.Printf("gRPC method: %s, %v", info.FullMethod, resp)
+	return resp, err
+}
+
 func main() {
-	server := grpc.NewServer()
+	opts := []grpc.ServerOption{
+		grpc_middleware.WithUnaryServerChain(
+			RecoveryInterceptor,
+			LoggingInterceptor,
+		),
+	}
+	server := grpc.NewServer(opts...)
 	pb.RegisterSearchServiceServer(server, &SearchService{})
 	listen, err := net.Listen("tcp", ":"+PORT)
 	if err != nil {
